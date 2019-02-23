2464dc0153b6ba48aec8d05534223ffed682d0ef
github username <github email address>
2019-02-23 11:54:44 -0500

    Validation in controller action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,16 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    @post = Post.find(params[:id])
+    @post.title = params[:title]
+    @post.category = params[:category]
+    @post.content = params[:content]
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
