8a807e43820af125608de0779ceaf14cd6564982
github username <github email address>
2019-02-25 17:34:10 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  # include ActiveModel::Validations
+  # validates_with MyValidator
+  # inclusion: %w(fiction non-fiction)
+
+  # , { with: "Fiction" || "Non-Fiction" }
+
+  # Subclassing ActiveModel::Validator and invoking with #validates_with
+
+
 end
